88bbb6f3619aef3181fab2c9eec469ded1bb1995
murega14 <tedmurega@gmail.com>
2024-08-10 11:55:07 +0300

    daraja api

--- app/mpesa.py
+++ app/mpesa.py
@@ -15,13 +15,13 @@ def generate_access_token():
     querystring = {"grant_type":"client_credentials"}
     payload = ""
     headers = {
-            "Authorization": "Basic {encoded_credentials}"
+            "Authorization": f"Basic {encoded_credentials}"
         }
     response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
     return response.text   
 
 
-def sendStkPush():
+def lipanampesa():
     token = generate_access_token()
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     shortCode = "9276285"
@@ -54,7 +54,7 @@ def sendStkPush():
     except Exception as e:
         print(f'Error: {str(e)}')
 
-def sendStkPush_familyBank():
+def lipanafamilybank():
     token = generate_access_token()
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     shortCode = '222111'
@@ -63,12 +63,56 @@ def sendStkPush_familyBank():
     stk_password = base64.b64decode((shortCode + passkey + timestamp).encode('utf-8')).decode('utf-8')
 
     headers = {
+        'Authorization': 'Bearer'+token,
         'Content-Type': 'application/json'
         }
     
     requestBody = {
         "BusinessShortCode": shortCode,
         "BillRefNumber": '752292',
+        "Password": stk_password,
         "TransactionType": "CustomerPaybillonline",
-
+        "Amount": 1,
+        "PartyA": 254741644151,
+        "PartyB": shortCode,
+        "CallbackURL": '',
+        "Transactiondesc": 'Test'
         }
+    
+    try:
+        response = requests.post(url, json=requestBody, headers=headers)
+        print(response.json())
+        return response.json()
+    except Exception as e:
+        print(f'Error: {str(e)}')
+
+def lipanacoop():
+    token = generate_access_token()
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    shortCode = '400200'
+    passkey = ''
+    url = ''
+    stk_password = base64.b64encode((shortCode + passkey +timestamp).encode('utf-8')).decode('utf-8')
+
+    headers = {
+        'Authorization': 'Bearer'+token,
+        'Content-Type': 'application/json'
+        }
+    
+    requestBody = {
+        "BusinessShortCode": shortCode,
+        "BillRefNumber": '37681',
+        "Passwword": stk_password,
+        "TransactionType": "CustomerPaybillOnline",
+        "Amount": 1,
+        "PartyA": 254741644151,
+        "PartyB": shortCode,
+        "CallbackURL": '',
+        "Transactiondesc": "Test"
+        }
+    
+    try:
+        response = requests.post(url, json=requestBody, headers=headers)
+        return response.json()
+    except Exception as e:
+        print(f'Error: {str(e)}')
