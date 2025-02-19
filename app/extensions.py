780787e86bb543fbb0c481755d91a367e48da9fa
Bravin Murega <tedmurega@gmail.com>
2025-02-19 18:05:51 +0300

    Implement Google OAuth2 signup and enhance business signup functionality; add error handling and session management

--- app/extensions.py
+++ app/extensions.py
@@ -2,6 +2,13 @@ import logging
 from itsdangerous import SignatureExpired, BadSignature, URLSafeTimedSerializer
 from flask import current_app
 import re
+from google_auth_oauthlib import Flow
+import tempfile
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
 
 logging = logging.basicConfig(
     level=logging.ERROR,
@@ -37,3 +44,24 @@ def validate_password(password):
     if not re.search(r"\d", password):
         return False
     return True
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+client_secrets_data = os.getenv("CLIENT_SECRETS")
+
+try:
+    client_secrets_dict = json.loads(client_secrets_data)
+except json.JSONDecodeError as e:
+    raise ValueError("client_Secrets contains invalid json") from e
+
+with tempfile.NamedTemporaryFile(delete=False,suffix=".json", mode="w") as temp_file:
+    temp_file_path = temp_file.name
+    json.dump(client_secrets_dict, temp_file)
+    
+flow = Flow.from_client_secrets_file(
+    temp_file_path,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="https://invotrack-2.onrender.com/callback"
+)
+
+os.unlink(temp_file_path)
