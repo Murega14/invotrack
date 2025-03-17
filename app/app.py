31cc9c21fc4fe26d61732b9c3f38b4e780266fda
Bravin Murega <tedmurega@gmail.com>
2025-03-17 15:18:58 +0300

    refractor: changed dashboard user identity to jwt

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from app import create_app
 from flask import render_template, session, flash, redirect, jsonify
 from.models import *
 from sqlalchemy import func
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from uuid import UUID
 
 app = create_app()
 
@@ -11,24 +13,26 @@ def index():
     return response, 200
 
 @app.route('/dashboard')
+@jwt_required()
 def dashboard():
-    google_id = session.get('google_id')
+    user_id = UUID(get_jwt_identity())
     
-    user = User.query.filter_by(google_id=google_id).first()
+    user = User.query.get(user_id)
     if not user:
-        flash('user not found')
-        return redirect('/login') 
+        return jsonify({"error": "user not found"}), 404
     user_invoices = Invoice.query.filter_by(user_id=user.id).all()
     
-    outstanding_invoices = Invoice.query.filter(Invoice.user_id == user.id, Invoice.status != 'paid').count()
-    total_paid = db.session.query(func.sum(Invoice.amount)).filter(Invoice.user_id == user.id, Invoice.status == 'paid').scalar() or 0
-    total_unpaid = db.session.query(func.sum(Invoice.amount)).filter(Invoice.user_id == user.id, Invoice.status != 'paid').scalar() or 0
+    outstanding_invoices = Invoice.query.filter(Invoice.issuer_id == user.id, Invoice.status != 'paid').count()
+    total_paid = db.session.query(func.sum(Invoice.total_amount)).filter(Invoice.issuer_id == user.id, Invoice.status == 'paid').scalar() or 0
+    total_unpaid = db.session.query(func.sum(Invoice.total_amount)).filter(Invoice.issuer_id == user.id, Invoice.status != 'paid').scalar() or 0
     
-    return render_template('dashboard.html',
-                           outstanding_invoices=outstanding_invoices, 
-                           total_paid="{:,.2f}".format(total_paid),
-                           total_unpaid="{:,.2f}".format(total_unpaid),
-                           user_invoices=user_invoices)
+    response = jsonify({
+        "success": True,
+        "outstanding_invoices": outstanding_invoices,
+        "total_paid": total_paid,
+        "total_unpaid": total_unpaid
+    })
+    return response, 200
 
 
 if __name__ == "__main__":
